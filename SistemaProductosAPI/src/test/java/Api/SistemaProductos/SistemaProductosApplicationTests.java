833595b29a327f8c0bf72f6a22a26e936e0ba287
Gastón Camú <125202280+GastonCamu@users.noreply.github.com>
2024-10-19 22:45:06 -0300

    Creacion base API

---
+++ SistemaProductosAPI/src/test/java/Api/SistemaProductos/SistemaProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package Api.SistemaProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
